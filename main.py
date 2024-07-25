1c0f269e19eeba211e90885b6b035415f169e5cc
Daniel <36015220+sosokokos@users.noreply.github.com>
2024-07-24 21:17:16 -0700

    final

--- main.py
+++ main.py
@@ -6,10 +6,6 @@ patents = ["US10244791B2", "US11399571B2", "US20210401061A1", "US10327479B2", "U
                "US20190045837A1", "US10765145B2", "USD871665S1", "US11462307B2", "US20120214107A1", 
                "US10251423B2", "US20050236006A1", "US4715387A", "US5893371A", "US20190387796A1"]
 
-
-
-
-
 def main():
     deleteIndex("test-index")
     initializeDatabase("test-index")
