44acecdb05980d61429d2c3925371a64c8650d68
Daniel <36015220+sosokokos@users.noreply.github.com>
2024-07-24 21:16:13 -0700

    mm

--- main.py
+++ main.py
@@ -6,14 +6,7 @@ patents = ["US10244791B2", "US11399571B2", "US20210401061A1", "US10327479B2", "U
                "US20190045837A1", "US10765145B2", "USD871665S1", "US11462307B2", "US20120214107A1", 
                "US10251423B2", "US20050236006A1", "US4715387A", "US5893371A", "US20190387796A1"]
 
-test_patents = ["US10513334B2", "US20190375505A1", "US10457379B2", "US4899766A", "US9095174B2", "US4941486A",
-                    "US4066088A", "US8186360B2", "US11503970B2", "US11287817B1", "US20200158527A1"]
 
-patents = patents + test_patents
-
-similar_patents = get_patent_numbers("US20190387796A1")
-
-patents = patents + similar_patents
 
 
 
@@ -21,6 +14,8 @@ def main():
     deleteIndex("test-index")
     initializeDatabase("test-index")
 
+    for patent in patents:
+        tester(patent)
 
     printResults(patents)
     
